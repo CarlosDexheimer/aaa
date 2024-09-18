676924eb96f1898a8215a7936a20e040bb48c9b9
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2024-09-18 22:57:00 +0000

    commit

---
+++ a.c
@@ -0,0 +1,23 @@
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int numprocs;
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    printf("\nNumero total de processos: %d", numprocs);
+
+    int myid;
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    printf("\nMeu id: %d\n", myid);
+
+    if (myid == 0) {
+        printf("\n\nMestre rodando id: %d\n\n", myid);
+    } else {
+        printf("\n\nEscravo rodando id: %d\n\n", myid);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
